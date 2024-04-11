b9ecc1732d13b85274c7f87e3b5ab660ca4c3b9a
bradencook <bradencook98@gmail.com>
2024-04-10 22:48:00 -0600

    more work on the controls stuff

--- scripts/screens/edit-controls.js
+++ scripts/screens/edit-controls.js
@@ -1,80 +1,37 @@
 MyGame.screens['edit-controls'] = (function(manager) {
     'use strict';
 
-    let running = false;
     let controlsList;
-    let image = new Image();
-    image.src = 'assets/arrow-4-way.svg'
-    image.isReady = false;  // Can't render until the texture is loaded
+    let upText;
+    let downText;
+    let leftText;
+    let rightText;
 
-    //
-    // Load the texture to use for the particle system loading and ready for rendering
-    image.onload = function() {
-        this.isReady = true;
-    }
+
 
     function initialize() {
         document.getElementById('id-controls-back').addEventListener(
             'click',
-            function() {running = false; manager.showScreen('main-menu'); });
+            function() {manager.showScreen('main-menu'); });
+
+        document.getElementById('up').addEventListener(
+            'click',
+            function() {editControl('up'); });
+        document.getElementById('down').addEventListener(
+            'click',
+            function() {editControl('down'); });
+        document.getElementById('left').addEventListener(
+            'click',
+            function() {editControl('left'); });
+        document.getElementById('right').addEventListener(
+            'click',
+            function() {editControl('right'); });
     }
 
-    let canvas = document.getElementById('id-controls-canvas');
-    let context = canvas.getContext('2d');
-
-    function isMouseOverText(mouseX, mouseY) {
-        return mouseX + mouseY > 0;
+    function editControl(direction) {
+        document.getElementById('instructions').textContent = 'Press a key to register'
+        document.getElementById(direction).style.color = 'red'
     }
-
-    // Mouse click event listener
-    canvas.addEventListener("click", function(event) {
-        var rect = canvas.getBoundingClientRect();
-        var mouseX = event.clientX - rect.left;
-        var mouseY = event.clientY - rect.top;
-
-        if (isMouseOverText(mouseX, mouseY)) {
-            alert("Text clicked!");
-            // Add your desired action here
-        }
-    });
-
-    function drawText(x, y, text, color) {
-        context.font = '20px Courier';
-        context.fillStyle = color;
-        context.textBaseline = 'top';
-        context.fillText(text, x, y);
-    }
-
-    function processInput() {
-
-    }
-
-    function render () {
-        if (image.isReady) {
-            context.drawImage(image, canvas.width/2 - canvas.height/2, 0,  canvas.height, canvas.height);
-        }
-
-        let textWidth = context.measureText(controlsList[0]).width;
-        drawText(canvas.width/2 - textWidth/2 - 20, 70, controlsList[0],  'white')
-
-        textWidth = context.measureText(controlsList[1]).width;
-        drawText(canvas.width/2 - textWidth/2, canvas.height - 90, controlsList[1],  'white')
-
-        textWidth = context.measureText(controlsList[2]).width;
-        drawText(canvas.width/2 - textWidth/2 - 120, canvas.height/2 - 10, controlsList[2],  'white')
-
-        textWidth = context.measureText(controlsList[3]).width;
-        drawText(canvas.width/2 - textWidth/2 + 120, canvas.height/2 - 10, controlsList[3],  'white')
-    }
-
-    function gameLoop() {
-        processInput();
-        render();
-
-        if (running) {
-            requestAnimationFrame(gameLoop);
-        }
-    };
 
     function run() {
         let running = true;
@@ -85,7 +42,16 @@ MyGame.screens['edit-controls'] = (function(manager) {
             controlsList = ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"]
         }
 
-        requestAnimationFrame(gameLoop);
+        upText = controlsList[0]
+        downText = controlsList[1]
+        leftText = controlsList[2]
+        rightText = controlsList[3]
+
+        document.getElementById('up').textContent = upText
+        document.getElementById('down').textContent = downText
+        document.getElementById('left').textContent = leftText
+        document.getElementById('right').textContent = rightText
+
     }
 
     return {
